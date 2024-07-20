9da0bf3078268d6cc9246244a42d1b01044fe149
khee2 <21keunghee@gmail.com>
2024-07-20 15:32:52 +0900

    initial setup: 프로젝트 초기설정 자바 17 & 스프링부트 3.3.1 세팅

---
+++ src/main/java/com/snowflake_guide/tourmate/TourmateApplication.java
@@ -0,0 +1,13 @@
+package com.snowflake_guide.tourmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowflake_guide/tourmate/TourmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowflake_guide.tourmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
