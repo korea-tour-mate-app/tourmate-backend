222c4f54174d98cbfa61c859371f82af7ae749d5
khee2 <21keunghee@gmail.com>
2024-08-21 23:47:55 +0900

    feat: Member 엔티티 생성

---
+++ src/main/java/com/snowflake_guide/tourmate/domain/member/entity/Member.java
@@ -0,0 +1,40 @@
+package com.snowflake_guide.tourmate.domain.member.entity;
+
+import com.snowflake_guide.tourmate.domain.like.entity.Like;
+import com.snowflake_guide.tourmate.domain.my_place.entity.MyPlace;
+import com.snowflake_guide.tourmate.global.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "member")
+    private List<Like> likes;
+
+    @OneToMany(mappedBy = "member")
+    private List<MyPlace> myPlaces;
+}
