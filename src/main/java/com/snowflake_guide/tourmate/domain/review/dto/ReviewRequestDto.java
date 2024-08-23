7213c4372c7b5b893febe62f991c622b105f7baa
khee2 <21keunghee@gmail.com>
2024-08-23 18:30:36 +0900

    fix: @NotBlank 대신 @NotNull을 사용하여 rate 비어있는 것 막기
    - @NotBlank은 문자열에만 사용할 수 있다

--- src/main/java/com/snowflake_guide/tourmate/domain/review/dto/ReviewRequestDto.java
+++ src/main/java/com/snowflake_guide/tourmate/domain/review/dto/ReviewRequestDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,8 +21,8 @@ public class ReviewRequestDto {
     private String reviewDec;
 
     @Schema(description = "평점", example = "4.5")
-    @NotBlank(message = "평점은 필수항목입니다.")
-    @Min(value = 0, message = "평점은 최소 0 이상이어야 합니다.")
-    @Max(value = 5, message = "평점은 최대 5 이하여야 합니다.")
+    @NotNull(message = "Rate는 필수 항목입니다.")
+    @Min(value = 0, message = "Rate는 0 이상이어야 합니다.")
+    @Max(value = 5, message = "Rate는 5 이하이어야 합니다.")
     private Float rate;
 }
