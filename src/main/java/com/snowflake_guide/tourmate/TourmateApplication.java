160d9d18d00d31f1273e5b524e8ddc390149b02a
khee2 <21keunghee@gmail.com>
2024-08-23 17:09:12 +0900

    fix: @EnableJpaAuditing를 추가함으로써 Spring Data JPA의 Auditing 기능 활성화
    - @CreatedDate와 @LastModifiedDate가 자동으로 저장되지 않는 문제 해결

--- src/main/java/com/snowflake_guide/tourmate/TourmateApplication.java
+++ src/main/java/com/snowflake_guide/tourmate/TourmateApplication.java
@@ -2,8 +2,10 @@ package com.snowflake_guide.tourmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TourmateApplication {
 
 	public static void main(String[] args) {
