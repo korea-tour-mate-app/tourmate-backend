a36df82e267531f350bb8f90acdd7152650f6cd6
khee2 <21keunghee@gmail.com>
2024-08-24 14:55:26 +0900

    feat: MyPlace와 VisitedPlace 엔티티 합치기
    - VisitedPlace에 visited, createdAt 추가
    - 방문상태 변경하는 toggleVisited추가
    - Member엔티티와 연결

--- src/main/java/com/snowflake_guide/tourmate/domain/visited_place/entity/VisitedPlace.java
+++ src/main/java/com/snowflake_guide/tourmate/domain/visited_place/entity/VisitedPlace.java
@@ -1,12 +1,13 @@
 package com.snowflake_guide.tourmate.domain.visited_place.entity;
 
-import com.snowflake_guide.tourmate.domain.my_place.entity.MyPlace;
+import com.snowflake_guide.tourmate.domain.member.entity.Member;
 import com.snowflake_guide.tourmate.domain.place.entity.Place;
 import com.snowflake_guide.tourmate.domain.review.entity.Review;
 import com.snowflake_guide.tourmate.global.util.BaseTimeEntity;
 import lombok.*;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -22,13 +23,23 @@ public class VisitedPlace extends BaseTimeEntity {
     private Long visitedPlaceId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "my_place_id", nullable = false)
-    private MyPlace myPlace;
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "place_id", nullable = false)
     private Place place;
 
+    @Column(nullable = false)
+    private Boolean visited; // 가본 유무
+
+    @Column(nullable = false)
+    private LocalDate createdAt = LocalDate.now(); // 생성일: 현재 날짜 할당되게 함
+
     @OneToMany(mappedBy = "visitedPlace")
     private List<Review> reviews;
+
+    public void toggleVisited(){
+        this.visited = !this.visited;
+    }
 }
