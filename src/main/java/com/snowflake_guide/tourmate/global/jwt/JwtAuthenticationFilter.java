6db3dbb15ccf1c712b1c47fe7f2bfd3b98fb3121
khee2 <21keunghee@gmail.com>
2024-08-27 21:27:36 +0900

    refactor 유효한 JWT 토큰의 사용자 정보가 null인 경우 경고 로그 추가

--- src/main/java/com/snowflake_guide/tourmate/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/snowflake_guide/tourmate/global/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.snowflake_guide.tourmate.global.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -28,14 +30,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
+        log.info("JWT Token: {}", token);
+
         if (token != null && jwtTokenProvider.validateToken(token)) {
             String username = jwtTokenProvider.getName(token);
+            log.info("Valid JWT token found for user: {}", username);
 
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            if (userDetails != null) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                log.warn("UserDetails is null for user: {}", username);
+            }
+        } else {
+            log.warn("Invalid JWT token or JWT token is null");
         }
 
         chain.doFilter(request, response);
