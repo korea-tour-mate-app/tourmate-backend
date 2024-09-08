fd2a4b27b89df3c1d7212327f5f622fa9de9816e
khee2 <21keunghee@gmail.com>
2024-09-08 16:40:20 +0900

    refactor: swagger tag 추가

--- src/main/java/com/snowflake_guide/tourmate/domain/member/api/SignInController.java
+++ src/main/java/com/snowflake_guide/tourmate/domain/member/api/SignInController.java
@@ -3,10 +3,12 @@ package com.snowflake_guide.tourmate.domain.member.api;
 import com.snowflake_guide.tourmate.domain.member.dto.SignInRequestDTO;
 import com.snowflake_guide.tourmate.domain.member.dto.SignInResponseDTO;
 import com.snowflake_guide.tourmate.domain.member.service.SignInService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "SingIn API", description = "자체 로그인하는 API")
 @RestController
 @RequestMapping("/api/auth/login")
 public class SignInController {
