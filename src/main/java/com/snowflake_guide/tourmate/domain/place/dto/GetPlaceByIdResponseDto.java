9760aecc8b77a19276b5dac5e5e402962d78357f
khee2 <21keunghee@gmail.com>
2024-09-23 05:16:39 +0900

    feat: 테마 상세정보 key값 한글로 반환

--- src/main/java/com/snowflake_guide/tourmate/domain/place/dto/GetPlaceByIdResponseDto.java
+++ src/main/java/com/snowflake_guide/tourmate/domain/place/dto/GetPlaceByIdResponseDto.java
@@ -1,6 +1,7 @@
 package com.snowflake_guide.tourmate.domain.place.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.snowflake_guide.tourmate.domain.place.entity.Place;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,31 +14,107 @@ import lombok.NoArgsConstructor;
 public class GetPlaceByIdResponseDto {
 
     private Long placeId;
+
     private Long themeId;
+
+    @JsonProperty("테마이름")
     private String placeTheme;
+
+    @JsonProperty("위도")
     private Float latitude;
+
+    @JsonProperty("경도")
     private Float longitude;
+
+    @JsonProperty("장소명")
     private String placeName;
+
+    @JsonProperty("주소명")
     private String placeLocation;
+
+    @JsonProperty("이미지")
     private String imageUrl1;
+
+    @JsonProperty("이미지2")
     private String imageUrl2;
+
+    @JsonProperty("이미지3")
     private String imageUrl3;
-    private String startEndTime;
-    private String deadlineTime;
-    private String placeCost;
-    private String checkInOutTime;
-    private String phoneNumber;
-    private String dayOff;
-    private String duringTime;
-    private Integer templateStayType;
+
+    @JsonProperty("영업시간")
+    private String time;
+
+    @JsonProperty("홈페이지 URL")
     private String homepageUrl;
-    private Float avgCost;
-    private Integer veganType;
-    private Integer foodType;
-    private Float rate;
-    private Integer viewType;
-    private Integer inoutType;
-    private Integer cafeType;
+
+    @JsonProperty("설명")
+    private String description;
+
+    @JsonProperty("평점")
+    private Float rating;
+
+    @JsonProperty("리뷰수")
+    private Integer reviewCount;
+
+    @JsonProperty("가격")
+    private Integer price;
+
+    @JsonProperty("비건 유형")
+    private String veganType;
+
+    @JsonProperty("음식 종류")
+    private String foodType;
+
+    @JsonProperty("방문 유형")
+    private String visitType;
+
+    @JsonProperty("실내외 유형")
+    private String indoorOutdoorType;
+
+    @JsonProperty("카페 유형")
+    private String cafeType;
+
+    @JsonProperty("입장료")
+    private String admissionFee;
+
+    @JsonProperty("체크인 시간")
+    private String checkInTime;
+
+    @JsonProperty("체크 아웃 시간")
+    private String checkOutTime;
+
+    @JsonProperty("숙박료")
+    private String dailyStayFee;
+
+    @JsonProperty("영업 시작 시간")
+    private String operationStartTime;
+
+    @JsonProperty("영업 종료 시간")
+    private String operationEndTime;
+
+    @JsonProperty("휴무일")
+    private String restDay;
+
+    @JsonProperty("관람 시간")
+    private String viewTime;
+
+    @JsonProperty("비고")
+    private String note;
+
+    @JsonProperty("이용 방법")
+    private String usage;
+
+    @JsonProperty("수영장 유무")
+    private Boolean hasSwimmingPool;
+
+    @JsonProperty("가격대")
+    private String priceRange;
+
+    @JsonProperty("관련 링크")
+    private String relatedLink;
+
+    @JsonProperty("총 별점")
+    private Integer totalRating;
 
     // Place 엔티티를 받아 필요한 필드만 초기화
     public static GetPlaceByIdResponseDto fromEntity(Place place) {
@@ -52,22 +129,31 @@ public class GetPlaceByIdResponseDto {
                 place.getImageUrl1(),
                 place.getImageUrl2(),
                 place.getImageUrl3(),
-                place.getStartEndTime(),
-                place.getDeadlineTime(),
-                place.getPlaceCost(),
-                place.getCheckInOutTime(),
-                place.getPhoneNumber(),
-                place.getDayOff(),
-                place.getDuringTime(),
-                place.getTemplateStayType() == 0 ? null : place.getTemplateStayType(),
+                place.getTime(),
                 place.getHomepageUrl(),
-                place.getAvgCost() == null || place.getAvgCost() == 0.0f ? null : place.getAvgCost(),
-                place.getVeganType() == 0 ? null : place.getVeganType(),
-                place.getFoodType() == 0 ? null : place.getFoodType(),
-                place.getRate() == null || place.getRate() == 0.0f ? null : place.getRate(),
-                place.getViewType() == 0 ? null : place.getViewType(),
-                place.getInoutType() == 0 ? null : place.getInoutType(),
-                place.getCafeType() == 0 ? null : place.getCafeType()
+                place.getDescription(),
+                place.getRating(),
+                place.getReviewCount(),
+                place.getPrice(),
+                place.getVeganType(),
+                place.getFoodType(),
+                place.getVisitType(),
+                place.getIndoorOutdoorType(),
+                place.getCafeType(),
+                place.getAdmissionFee(),
+                place.getCheckInTime(),
+                place.getCheckOutTime(),
+                place.getDailyStayFee(),
+                place.getOperationStartTime(),
+                place.getOperationEndTime(),
+                place.getRestDay(),
+                place.getViewTime(),
+                place.getNote(),
+                place.getUsage(),
+                place.getHasSwimmingPool(),
+                place.getPriceRange(),
+                place.getRelatedLink(),
+                place.getTotalRating()
         );
     }
 }
--- src/main/java/com/snowflake_guide/tourmate/domain/place/entity/Place.java
+++ src/main/java/com/snowflake_guide/tourmate/domain/place/entity/Place.java
@@ -25,43 +25,91 @@ public class Place extends BaseTimeEntity {
     private Theme theme;
 
     @Column(nullable = false)
-    private Float latitude;
+    private Float latitude; // 위도
 
     @Column(nullable = false)
-    private Float longitude;
+    private Float longitude; // 경도
 
     @Column(nullable = false)
-    private String placeName; // 여행지
+    private String placeName; // 여행지 이름
 
     @Column(nullable = false, length = 300)
     private String placeLocation; // 주소
 
-    @Column(nullable = false, length = 300)
+    @Column(length = 300)
     private String imageUrl1; // 장소 이미지1
     private String imageUrl2; // 장소 이미지2
     private String imageUrl3; // 장소 이미지3
-    private String startEndTime; // 영업시간(영업 시작 ~ 종료시간)
 
-    @Column(nullable = false, length = 300)
+    private String time; // 영업시간(영업 시작 ~ 종료시간)
+
+    @Column(length = 300)
     private String homepageUrl; // 홈페이지 주소
 
-    private String deadlineTime; // 입장마감 시간
-    private String placeCost; // 입장료
-    private String checkInOutTime; // 체크인, 체크아웃시간
-    private String phoneNumber; // 전화번호
-    private String dayOff; // 휴무일
-    private String duringTime; // 관람시간
-//    private String stayInCost; // 하루숙박료
+    @Column(length = 500)
+    private String description; // 설명
 
+    private Float rating; // 별점
 
-    private int templateStayType; // 템플스테이유형
-    private Float avgCost; // 평균 가격
-    private int veganType; // 비건유형
-    private int foodType; // 음식유형
-    private Float rate; // 별점
-    private int viewType; // 관람유형
-    private int inoutType; // 실내외유형
-    private int cafeType; // 카페유형
+    private Integer reviewCount; // 리뷰 개수
+
+    private Integer price; // 평균 가격
+
+    @Column(length = 500)
+    private String veganType; // 비건 유형
+
+    @Column(length = 500)
+    private String foodType; // 음식 종류 유형
+
+    @Column(length = 500)
+    private String visitType; // 관람 유형
+
+    @Column(length = 500)
+    private String indoorOutdoorType; // 실내외 유형
+
+    @Column(length = 500)
+    private String cafeType; // 카페 유형
+
+    @Column(length = 500)
+    private String admissionFee; // 입장료
+
+    @Column(length = 500)
+    private String checkInTime; // 체크인 시간
+
+    @Column(length = 500)
+    private String checkOutTime; // 체크아웃 시간
+
+    @Column(length = 500)
+    private String dailyStayFee; // 하루 숙박료
+
+    @Column(length = 500)
+    private String operationStartTime; // 영업 시작 시간
+
+    @Column(length = 500)
+    private String operationEndTime; // 영업 종료 시간
+
+    @Column(length = 500)
+    private String restDay; // 휴무일
+
+    @Column(length = 500)
+    private String viewTime; // 관람 시간
+
+    @Column(length = 500)
+    private String note; // 비고
+
+    @Column(length = 500)
+    private String usage; // 이용 방법
+
+    private Boolean hasSwimmingPool; // 수영장 유무
+
+    @Column(length = 500)
+    private String priceRange; // 가격대
+
+    @Column(length = 1000)
+    private String relatedLink; // 관련 링크
+
+    @Column(length = 500)
+    private Integer totalRating; // 총 별점
 
     @OneToMany(mappedBy = "place")
     private List<Like> likes;
@@ -69,4 +117,3 @@ public class Place extends BaseTimeEntity {
     @OneToMany(mappedBy = "place")
     private List<VisitedPlace> visitedPlaces;
 }
-
